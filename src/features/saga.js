3a458d9304b8573ad2742e3dc9a714fd8493e713
Chan <lhc0506@gmail.com>
2022-02-06 18:11:13 +0900

    [01] 로그인 페이지 (#2)
    * :[01] 로그인 페이지
    
    - Feat: 로그인 페이지 UI
    - firebase 로그인 연동
    - 백엔드 로그인 연동

---
+++ src/features/saga.js
@@ -0,0 +1,63 @@
+import { put, takeLatest, all, fork, call } from "redux-saga/effects";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { loginRequest, loginSucceed, loginFailed, join } from "./userSlice";
+import { authentication } from "./firebase";
+import axios from "axios";
+
+export default function* rootSaga() {
+  yield all([fork(watchLogin), fork(watchJoin)]);
+}
+
+function* watchLogin() {
+  yield takeLatest(loginRequest, login);
+}
+
+function* watchJoin() {
+  yield takeLatest(join, login, "join");
+}
+
+async function signInGoogle() {
+  const provieder = new GoogleAuthProvider();
+  const googleLoginResult = await signInWithPopup(authentication, provieder);
+  const { email, displayName, photoURL: profile } = googleLoginResult.user;
+  return { email, displayName, profile };
+}
+
+async function loginServer(email) {
+  const res = await axios.post("/api/auth/login", { email });
+  return res;
+}
+
+async function joinServer(user) {
+  const res = await axios.post("/api/users", { user });
+  return res;
+}
+
+function* login(action) {
+  try {
+    const signInGoogleResult = yield call(signInGoogle);
+
+    if (signInGoogleResult && action === "join") {
+      yield call(joinServer, signInGoogleResult);
+    }
+
+    const serverLoginResult = yield call(loginServer, signInGoogleResult.email);
+
+    if (serverLoginResult.data.isSuccess) {
+      const { email, displayName, profile, newAccessToken } =
+        serverLoginResult.data;
+      yield put(loginSucceed({ email, displayName, profile }));
+      axios.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${newAccessToken}`;
+    } else {
+      yield put(
+        loginFailed({
+          name: "ServerLoginFailed",
+        }),
+      );
+    }
+  } catch (error) {
+    yield put(loginFailed(error));
+  }
+}
---
+++ src/features/userSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  loginLoading: false,
+  loginSucceed: false,
+  loginError: null,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    loginRequest: state => {
+      state.loginLoading = true;
+      state.loginSucceed = false;
+      state.loginError = null;
+    },
+    loginSucceed: (state, action) => {
+      state.loginLoading = false;
+      state.loginSucceed = true;
+      state.loginError = null;
+      console.log(111, action);
+      state.user = action.payload;
+    },
+    loginFailed: (state, action) => {
+      state.loginLoading = false;
+      state.loginSucceed = false;
+      state.loginError = action.payload;
+    },
+    join: state => {
+      state.loginLoading = true;
+      state.loginSucceed = false;
+      state.loginError = null;
+    },
+  },
+});
+
+export const { loginRequest, loginSucceed, loginFailed, join } =
+  userSlice.actions;
+export default userSlice.reducer;
