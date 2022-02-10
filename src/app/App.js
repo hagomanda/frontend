b410e30945274b08b9500ad788eb90c95c649732
Chan <lhc0506@gmail.com>
2022-02-10 09:55:49 +0900

    [15] 달력페이지 (#9)
    [15] 달력페이지
    
    - Feat: 달력 UI 생성
    - 주간 이동 구현
    - 오늘 클릭시 오늘 날자 이동구현
    - 서버에서 해당 주간 todos data get 구현

--- src/app/App.js
+++ src/app/App.js
@@ -6,11 +6,12 @@ import styled from "styled-components";
 import Navbar from "../Navbar";
 import { refresh } from "../features/userSlice";
 
-import MainGoalPage from "../components/MainGoalPage";
+import MainMandal from "../components/MainMandal";
 import axios from "axios";
 import MandalPage from "../components/MandalPage";
 import LoginPage from "../components/LoginPage";
 import MainGoalLists from "../components/mainGoal/MainGoalLists";
+import CalendarPage from "../components/CalendarPage";
 
 axios.defaults.withCredentials = true;
 
@@ -32,10 +33,11 @@ function App() {
         {/* <Route path="/" element={} /> */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/mainGoal/:id" element={<MandalPage />} />
+        <Route path="/calendar" element={<CalendarPage />} />
       </Routes>
       <AppContainer>
-        <MainGoalLists />
-        <Navbar />
+        {/* <MainGoalLists /> */}
+        {/* <Navbar /> */}
       </AppContainer>
     </BrowserRouter>
   );
---
+++ src/components/CalendarPage.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { format, add, getDay } from "date-fns";
+import CalendarBox from "./CalendarBox";
+import styled from "styled-components";
+import axios from "axios";
+
+const Container = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+  justify-content: center;
+  align-items: center;
+  border-collapse: collapse;
+`;
+
+export default function CalendarPage() {
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [todos, setTodos] = useState([]);
+  const loginState = useSelector(state => state.user.loginSucceed);
+
+  const WEEK = ["일", "월", "화", "수", "목", "금", "토"];
+  const handlePrevButtonClick = () => {
+    setCurrentDate(add(currentDate, { days: -7 }));
+  };
+  const handleNextButtonClick = () => {
+    setCurrentDate(add(currentDate, { days: 7 }));
+  };
+
+  const goToday = () => {
+    setCurrentDate(new Date());
+  };
+
+  const getUsersTodo = async id => {
+    const weekStart = add(currentDate, { days: -1 * getDay(currentDate) });
+    const res = await axios.get("/api/todos", {
+      headers: {
+        currentDate: format(weekStart, "yyyy-MM-dd"),
+      },
+    });
+
+    if (res.data.result === "error") {
+      return;
+    }
+
+    setTodos(res.data.result);
+  };
+
+  const showCalendar = () => {
+    return WEEK.map((day, i) => {
+      const date = format(
+        add(currentDate, { days: -1 * getDay(currentDate) + i }),
+        "yyyy-MM-dd",
+      );
+
+      return (
+        <CalendarBox day={day} date={date} key={date} todo={todos?.[date]} />
+      );
+    });
+  };
+
+  useEffect(() => {
+    if (loginState) {
+      getUsersTodo();
+    }
+  }, [loginState]);
+
+  return (
+    <div>
+      <Container>
+        <div onClick={goToday}>오늘</div>
+        <div onClick={handlePrevButtonClick}>{`<`}</div>
+        {format(currentDate, "yyyy년 MM월")}
+        <div onClick={handleNextButtonClick}>{`>`}</div>
+      </Container>
+      <Container>{showCalendar()}</Container>
+    </div>
+  );
+}
