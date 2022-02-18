6e0bd607b29c0a5aa97c2c931d65d1fab0564146
Chan <lhc0506@gmail.com>
2022-02-19 06:46:16 +0900

    transport값_제거

--- src/features/socket.js
+++ src/features/socket.js
@@ -1,6 +1,5 @@
 import io from "socket.io-client";
 export const socket = io.connect(process.env.REACT_APP_SERVER_URL, {
-  transports: ["websocket"],
   cors: { origin: process.env.REACT_APP_SERVER_URL },
 });
 
